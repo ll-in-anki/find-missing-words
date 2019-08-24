5694c20f46c63a9a94ebcb175cde0efba2551f7e
Connor Finley <connor.finley5@gmail.com>
2019-08-24 16:54:56 -0400

    Add filter for fields in find missing words tag

--- views/find_missing.py
+++ views/find_missing.py
@@ -1,7 +1,11 @@
+from functools import partial
+
 from aqt import mw
 from aqt.qt import *
 import aqt.deckchooser
 import aqt.modelchooser
+
+from . import field_chooser
 
 
 class FindMissingWords(QVBoxLayout):
@@ -9,58 +13,50 @@ class FindMissingWords(QVBoxLayout):
         super().__init__()
         self.deck_selection_enabled = True
         self.model_selection_enabled = False
+        self.field_selection_enabled = False
         self.render()
 
     def render(self):
-        self.render_deck_chooser()
-        self.render_model_chooser()
+        self.deck_chooser = self.render_filter("Filter deck?", self.deck_selection_enabled, self.toggle_deck_selection, aqt.deckchooser.DeckChooser)
+        self.model_chooser = self.render_filter("Filter model/note type?", self.model_selection_enabled, self.toggle_model_selection, aqt.modelchooser.ModelChooser)
+        self.field_chooser = self.render_filter("Filter field?", self.field_selection_enabled, self.toggle_field_selection, field_chooser.FieldChooser)
         self.render_text_area()
         self.render_search_button()
+
+    def render_filter(self, checkbox_label, state, on_state_change, chooser_class):
+        checkbox = QCheckBox(checkbox_label)
+        checkbox.setChecked(state)
+
+        chooser_widget = QWidget()
+        chooser = chooser_class(mw, chooser_widget)
+        chooser_widget.setEnabled(state)
+        checkbox.stateChanged.connect(partial(on_state_change, chooser_widget))
+
+        btn_layout = QHBoxLayout()
+        btn_layout.addWidget(checkbox)
+        btn_layout.addStretch(1)
+        btn_layout.addWidget(chooser_widget)
+        self.addLayout(btn_layout)
+        return chooser
+
+    def toggle_deck_selection(self, parent_widget):
+        self.deck_selection_enabled = not self.deck_selection_enabled
+        parent_widget.setEnabled(self.deck_selection_enabled)
+
+    def toggle_model_selection(self, parent_widget):
+        self.model_selection_enabled = not self.model_selection_enabled
+        parent_widget.setEnabled(self.model_selection_enabled)
+
+    def toggle_field_selection(self, parent_widget):
+        self.field_selection_enabled = not self.field_selection_enabled
+        parent_widget.setEnabled(self.field_selection_enabled)
 
     def render_text_area(self):
         self.text_area_label = QLabel()
         self.text_area_label.setText("Text:")
         self.addWidget(self.text_area_label)
-        self.text = QTextEdit()
-        self.addWidget(self.text)
-
-    def render_deck_chooser(self):
-        self.deck_selection_checkbox = QCheckBox("Filter Deck?")
-        self.deck_selection_checkbox.setChecked(self.deck_selection_enabled)
-        self.deck_selection_checkbox.stateChanged.connect(self.toggle_deck_selection)
-
-        self.deck_chooser_parent_widget = QWidget()
-        self.deck_chooser = aqt.deckchooser.DeckChooser(mw, self.deck_chooser_parent_widget)
-        self.deck_chooser_parent_widget.setEnabled(self.deck_selection_enabled)
-
-        btn_layout = QHBoxLayout()
-        btn_layout.addWidget(self.deck_selection_checkbox)
-        btn_layout.addStretch(1)
-        btn_layout.addWidget(self.deck_chooser_parent_widget)
-        self.addLayout(btn_layout)
-
-    def render_model_chooser(self):
-        self.model_selection_checkbox = QCheckBox("Filter Model/Note Type?")
-        self.model_selection_checkbox.setChecked(self.model_selection_enabled)
-        self.model_selection_checkbox.stateChanged.connect(self.toggle_model_selection)
-
-        self.model_chooser_parent_widget = QWidget()
-        self.model_chooser = aqt.modelchooser.ModelChooser(mw, self.model_chooser_parent_widget)
-        self.model_chooser_parent_widget.setEnabled(self.model_selection_enabled)
-
-        btn_layout = QHBoxLayout()
-        btn_layout.addWidget(self.model_selection_checkbox)
-        btn_layout.addStretch(1)
-        btn_layout.addWidget(self.model_chooser_parent_widget)
-        self.addLayout(btn_layout)
-
-    def toggle_deck_selection(self):
-        self.deck_selection_enabled = not self.deck_selection_enabled
-        self.deck_chooser_parent_widget.setEnabled(self.deck_selection_enabled)
-
-    def toggle_model_selection(self):
-        self.model_selection_enabled = not self.model_selection_enabled
-        self.model_chooser_parent_widget.setEnabled(self.model_selection_enabled)
+        self.text_area_text = QTextEdit()
+        self.addWidget(self.text_area_text)
 
     def render_search_button(self):
         self.search_button = QPushButton("Search")
@@ -72,12 +68,8 @@ class FindMissingWords(QVBoxLayout):
 
     def search(self):
         # Search through fetched cards here
-        deck_id = self.deck_chooser.selectedId()
-        self.deck_label = QLabel()
-        self.deck_label.setText(str(deck_id))
-        self.addWidget(self.deck_label)
+        deck_id = self.deck_selection_enabled and self.deck_chooser.selectedId()
+        deck_name = self.deck_selection_enabled and self.deck_chooser.deckName()
+        model_name = self.model_selection_enabled and mw.col.models.current()['name']
+        field_name = self.field_selection_enabled and self.field_chooser.current_field_name
 
-        # model_id = self.model_chooser.selectedId()
-        # self.model_label = QLabel()
-        # self.model_label.setText(str(model_id))
-        # self.addWidget(self.model_label)
