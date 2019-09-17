120eaf588da36676a46d19a96cbef2917cc70501
Connor Finley <connor.finley5@gmail.com>
2019-09-17 14:00:56 -0400

    Add AnkiAddonReloader hook for quicker testing

--- src/find_missing_words/__init__.py
+++ src/find_missing_words/__init__.py
@@ -1,5 +1,12 @@
-def initialize_addon():
+def addon_reloader_after():
+    """
+    Add hook for AnkiAddonReloader to auto-show the addon after reloading to save clicks
+    """
+    from .gui.options import invoke_addon_window
+    invoke_addon_window()
 
+
+def initialize_addon():
     from .gui.options import initialize_menu_item
     initialize_menu_item()
 
