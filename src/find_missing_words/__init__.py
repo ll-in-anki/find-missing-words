08148ac9b0d758bddd49fc11001f5cb8ade0fd5c
Jordan Munch O'Hare <ohare93@gmail.com>
2019-11-25 21:30:00 +0100

    First attempts at unit testing (need further Form mocks)

--- src/find_missing_words/__init__.py
+++ src/find_missing_words/__init__.py
@@ -1,23 +1,27 @@
-def addon_reloader_before():
-    """
-    Remove any instance(s) of the addon in the Anki Tools menu to prevent duplicates
-    """
-    from .gui.options import cleanup_menu_items
-    cleanup_menu_items()
+
+# print(f"Name is: '{__name__}'")
+if __name__ != "src.find_missing_words":
+
+    def addon_reloader_before():
+        """
+        Remove any instance(s) of the addon in the Anki Tools menu to prevent duplicates
+        """
+        from .gui.options import cleanup_menu_items
+        cleanup_menu_items()
 
 
-def addon_reloader_after():
-    """
-    Add hook for AnkiAddonReloader to auto-show the addon after reloading to save clicks
-    """
-    from .gui.options import invoke_addon_window
-    invoke_addon_window()
+    def addon_reloader_after():
+        """
+        Add hook for AnkiAddonReloader to auto-show the addon after reloading to save clicks
+        """
+        from .gui.options import invoke_addon_window
+        invoke_addon_window()
 
 
-def initialize_addon():
-    from .gui.options import initialize_menu_item, initialize_config_menu_item
-    initialize_menu_item()
-    initialize_config_menu_item()
+    def initialize_addon():
+        from .gui.options import initialize_menu_item, initialize_config_menu_item
+        initialize_menu_item()
+        initialize_config_menu_item()
 
 
-initialize_addon()
+    initialize_addon()
--- src/find_missing_words/gui/forms/__init__.py
+++ src/find_missing_words/gui/forms/__init__.py
@@ -1,1 +1,4 @@
-from .anki21 import *
+
+#print(f"Name is: '{__name__}'")
+if __name__ != "src.find_missing_words.gui.forms":
+    from .anki21 import *
